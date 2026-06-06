39fb7db0b0bbaac7f418bdaa0ef5e21794ab0c3b
Yvonne Maigo <Cynthiayvonne07@gmail.com>
2026-06-06 13:18:06 +0300

    fix barcode generator

--- tool/vendor/vendor/picqer/php-barcode-generator/src/BarcodeGenerator.php
+++ tool/vendor/vendor/picqer/php-barcode-generator/src/BarcodeGenerator.php
@@ -1001,7 +1001,7 @@ abstract class BarcodeGenerator
         $k = 0;
         for ($i = 0; $i < $len; ++$i) {
             $w += 1;
-            if (($i == ($len - 1)) OR (($i < ($len - 1)) AND ($seq[$i] != $seq{($i + 1)}))) {
+            if (($i == ($len - 1)) OR (($i < ($len - 1)) AND ($seq[$i] != $seq[($i + 1)]))) {
                 if ($seq[$i] == '1') {
                     $t = true; // bar
                 } else {
@@ -1647,7 +1647,7 @@ abstract class BarcodeGenerator
         $w = 0;
         for ($i = 0; $i < $clen; ++$i) {
             $w += 1;
-            if (($i == ($clen - 1)) OR (($i < ($clen - 1)) AND ($seq[$i] != $seq{($i + 1)}))) {
+            if (($i == ($clen - 1)) OR (($i < ($clen - 1)) AND ($seq[$i] != $seq[($i + 1)]))) {
                 if ($seq[$i] == '1') {
                     $t = true; // bar
                 } else {
